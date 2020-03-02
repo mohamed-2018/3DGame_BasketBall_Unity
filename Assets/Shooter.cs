2a141fb76878983161cc6df95e6d6806129ec3a7
Mohamed-2018 <mohamed2018ced@gmail.com>
2020-03-02 09:52:43 +0200

    First commit

---
+++ Assets/Shooter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    class Shooter:MonoBehaviour
+    {
+       public Rigidbody prefab;
+       public float speed = 10.0f;
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            var obj = (Rigidbody)Instantiate(this.prefab, transform.position, transform.rotation);
+            obj.velocity = (transform.forward + transform.up / 2) * speed;
+        }
+    }
+
+}
